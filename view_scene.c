2f59fe8ac1650d6fddd197aed00ab0f803eefd01
Elliot Cross <elliotcross27@gmail.com>
2020-02-08 17:45:02 +0100

    added basic shading

--- view_scene.c
+++ view_scene.c
@@ -6,7 +6,7 @@
 /*   By: ecross <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 17:03:58 by ecross            #+#    #+#             */
-/*   Updated: 2020/02/08 12:43:10 by ecross           ###   ########.fr       */
+/*   Updated: 2020/02/08 17:44:13 by ecross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,22 +93,66 @@ void	calc_3d_vector(double *start, double *end, double *res)
 	res[2] = end[2] - start[2];
 }
 
-double	calc_light_intensity()
+double	calc_vector_mag(double *vec)
 {
-	/*
-	  compute t_min coordinates
-	  compute vector from t_min to light
-	  compute sphere surface normal
-	  compute dot product of normal and light ray
-	  compute intensity from spot light
-	  add to ambient intensity
-	  return overall intensity value
-	 */
+	return(sqrt((vec[0] * vec[0]) + (vec[1] * vec[1]) + (vec[2] * vec[2])));
 }
 
-double	solve_quadratic(double *cam_xyz, double *ray_vector, double *sphere_xyz, double sphere_diameter)
+void	calc_unit_vec(double *vec, double *unit_vec)
 {
-	double	sphere_to_cam_vector[3];
+	double mag;
+
+	mag = calc_vector_mag(vec);;
+	unit_vec[0] = vec[0] / mag;
+	unit_vec[1] = vec[1] / mag;
+	unit_vec[2] = vec[2] / mag;
+}
+
+double	calc_light_intensity(double *ray_vec, scene_struct *s, double t_min)
+{
+	/*compute surface point coordinates*/
+	/*cam pos + t_min(unit ray vector)*/
+
+	double	dot;
+	double	fraction;
+	double	ray_unit_vec[3];
+	double	obj_surface_xyz[3];
+	double	surface_to_light_vec[3];
+	double	obj_norm_vec[3];
+	
+	calc_unit_vec(ray_vec, ray_unit_vec);
+	obj_surface_xyz[X] = s->cam_xyz[X] + (t_min * ray_unit_vec[X]);
+	obj_surface_xyz[Y] = s->cam_xyz[Y] + (t_min * ray_unit_vec[Y]);
+	obj_surface_xyz[Z] = s->cam_xyz[Z] + (t_min * ray_unit_vec[Z]);
+
+	/*compute vector from surface pos to light*/
+
+	calc_3d_vector(obj_surface_xyz, s->light_xyz, surface_to_light_vec);
+
+	/*compute surface normal (normalised) - need identifier of which object - use function array*/
+	/*here using sphere so normal is just vector between center and surface point*/
+	/*normal calculation functions should always give a normalised vector*/
+	
+	calc_3d_vector(s->sphere_xyz, obj_surface_xyz, obj_norm_vec);
+	calc_unit_vec(obj_norm_vec, obj_norm_vec);
+	
+	/*compute dot product of normal and light ray*/
+	/*if the dot product is negative, means angle between vectors of more than 90, which
+	  means light source is behind the point - so no illumination*/
+
+	if ((dot = calc_dot_prod(surface_to_light_vec, obj_norm_vec)) < 0)
+		return (0);
+
+	/*compute intensity from spot light*/
+	fraction = dot / (calc_vector_mag(surface_to_light_vec) * calc_vector_mag(obj_norm_vec));
+	
+	/*return this fraction of spot light intensity plus ambient light intensity*/
+	return (s->light_brightness * fraction);
+}
+
+double	solve_quadratic(double *cam_xyz, double *ray_vec, double *sphere_xyz, double sphere_diameter)
+{
+	double	sphere_to_cam_vec[3];
 	double	a;
 	double	b;
 	double	c;
@@ -116,11 +160,11 @@ double	solve_quadratic(double *cam_xyz, double *ray_vector, double *sphere_xyz, 
 	double	discriminant;
 	double	smallest_root;
 
-	calc_3d_vector(sphere_xyz, cam_xyz, sphere_to_cam_vector);
+	calc_3d_vector(sphere_xyz, cam_xyz, sphere_to_cam_vec);
 	r = sphere_diameter / 2;
-	a = calc_dot_prod(ray_vector, ray_vector);
-	b = 2 * calc_dot_prod(sphere_to_cam_vector, ray_vector);
-	c = calc_dot_prod(sphere_to_cam_vector, sphere_to_cam_vector) - (r * r);
+	a = calc_dot_prod(ray_vec, ray_vec);
+	b = 2 * calc_dot_prod(sphere_to_cam_vec, ray_vec);
+	c = calc_dot_prod(sphere_to_cam_vec, sphere_to_cam_vec) - (r * r);
 	discriminant = (b * b) - (4 * a * c);
 	if (discriminant < 0)
 		return (INFINITY);
@@ -147,7 +191,7 @@ int main(void)
 	double	viewport_width;
 	double	viewport_height;
 	double	viewport_pix_xyz[3];
-	double	ray_vector[3];
+	double	ray_vec[3];
 	double	intersect_dist_max;
 	double	intersect_dist_min;
 	double	t_min;
@@ -173,9 +217,9 @@ int main(void)
 	s.ambient_colour[Y] = 0;
 	s.ambient_colour[Z] = 0;
 
-	s.light_xyz[X] = 0;
-	s.light_xyz[Y] = 0;
-	s.light_xyz[Z] = 0;
+	s.light_xyz[X] = 50;
+	s.light_xyz[Y] = 30;
+	s.light_xyz[Z] = 50;
 	s.light_brightness = 0.5;
 	s.light_colour[X] = 255;
 	s.light_colour[Y] = 255;
@@ -222,27 +266,29 @@ int main(void)
 	  ray tracing algorithm
 	*/
 
-	pixel_colour[R] = 255;
-	pixel_colour[G] = 120;
-	pixel_colour[B] = 100;
+	pixel_colour[R] = 0;
+	pixel_colour[G] = 0;
+	pixel_colour[B] = 0;
 	t_min = INFINITY;
-	ray_vector[Z] = distance_to_viewport;
+	ray_vec[Z] = distance_to_viewport;
 	x = 0;
 	while (x < s.res_xy[X])
 	{
 		y = 0;
 		while (y < s.res_xy[Y])
 		{
-			ray_vector[X] = (viewport_width * ((double)x / s.res_xy[X])) - (viewport_width / 2);
-			ray_vector[Y] = (-1 * viewport_height * ((double)y / s.res_xy[Y])) + (viewport_height / 2);
+			ray_vec[X] = (viewport_width * ((double)x / s.res_xy[X])) - (viewport_width / 2);
+			ray_vec[Y] = (-1 * viewport_height * ((double)y / s.res_xy[Y])) + (viewport_height / 2);
 			/*for each object in object list - find t_min, if this is the smallest found so far
 			  store it in t_min var and keep track of which object this was*/
-			t_min = solve_quadratic(s.cam_xyz, ray_vector, s.sphere_xyz, s.sphere_diameter);
+			t_min = solve_quadratic(s.cam_xyz, ray_vec, s.sphere_xyz, s.sphere_diameter);
 			if (t_min == INFINITY)
 				colour_img_pixel(img_addr, x, y, bpp, line_size, s.ambient_colour);
 			else
 			{
 				/*do lots of stuff to get correct colour - using t_min and the object which gave it*/
+				pixel_colour[G] = (double)255 * (s.ambient_ratio + calc_light_intensity(ray_vec, &s, t_min));
+				pixel_colour[R] = (double)255 * (s.ambient_ratio + calc_light_intensity(ray_vec, &s, t_min));
 				colour_img_pixel(img_addr, x, y, bpp, line_size, pixel_colour);
 			}
 			y++;
