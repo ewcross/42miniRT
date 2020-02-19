0aa60e6592bb4811f5df75311f9797bb45929378
Elliot Cross <elliotcross27@gmail.com>
2020-02-19 20:30:00 +0100

    starting with square

--- obj_intercepts.c
+++ obj_intercepts.c
@@ -6,7 +6,7 @@
 /*   By: ecross <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 11:48:54 by ecross            #+#    #+#             */
-/*   Updated: 2020/02/19 18:46:46 by ecross           ###   ########.fr       */
+/*   Updated: 2020/02/19 20:29:41 by ecross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,10 +61,24 @@ int	solve_quadratic(double *t_min, double *ray_vec, double *ray_orig_xyz,
 }
 
 int	sq_intercept(double *t_min, double *ray_vec, double *ray_orig_xyz,
-				t_obj_struct *pl)
+				t_obj_struct *sq)
 {
-	*t_min = INFINITY;
-	return (0);
+	double	ray_normal_dot;
+	double	plane_to_cam_vec[3];
+	double	obj_surface_xyz[3];
+
+	calc_3d_vector(ray_orig_xyz, sq->xyz, plane_to_cam_vec);
+	ray_normal_dot = calc_dot_prod(ray_vec, sq->normal);
+	if (!ray_normal_dot)
+	{
+		*t_min = INFINITY;
+		return (0);
+	}
+	*t_min = calc_dot_prod(plane_to_cam_vec, sq->normal) / ray_normal_dot;
+	obj_surface_xyz[X] = cam->xyz[X] + (*t_min * ray_vec[X]);
+	obj_surface_xyz[Y] = cam->xyz[Y] + (*t_min * ray_vec[Y]);
+	obj_surface_xyz[Z] = cam->xyz[Z] + (*t_min * ray_vec[Z]);
+	return (1);
 }
 
 int	tr_intercept(double *t_min, double *ray_vec, double *ray_orig_xyz,
